bba6fd8a548509f373cf3a2bc14f1ceacf08b97c
kubanychbek11 <kuba.seitkaziev@gmail.com>
2023-02-24 20:34:16 +0600

    'hw4'

--- show_book/urls.py
+++ show_book/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns =[
     path('showbook/', views.show_bookview, name='show'),
     path('showbook/<int:id>/', views.show_book_detailview, name='details'),
+    path('showbook/<int:id>/update/', views.update_show_book_view, name='update'),
+    path('showbook/<int:id>/delete/', views.delete_show_book_view, name='delete'),
+    path('add-book/', views.create_show_book_view, name='create'),
+
 
 ]
