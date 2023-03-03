94f4c70b9a5e814825f865752d9b2c0684d1c717
kubanychbek11 <kuba.seitkaziev@gmail.com>
2023-03-03 17:32:49 +0600

    'hw6'

--- main/settings.py
+++ main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'book',
     'show_book',
+    'parser_py',
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('book.urls')),
     path('', include('show_book.urls')),
+    path('', include('parser_py.urls')),
 
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
